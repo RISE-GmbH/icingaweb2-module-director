e65e70ab12f6c9efe988d3d15de79390f7aeacc4
MoreAmazingNick <github@nicolas-schneider.at>
2023-10-19 17:31:16 +0200

    wrong-scheduled-downtime-url-in-activitylog

--- library/Director/Web/Table/ActivityLogTable.php
+++ library/Director/Web/Table/ActivityLogTable.php
@@ -213,6 +213,13 @@ class ActivityLogTable extends ZfQueryBasedTable
             // multi column key :(
             if ($type === 'service' || $this->hasObjectFilter) {
                 $object = "\"$name\"";
+            } elseif ($type === 'scheduled_downtime') {
+                $object = Link::create(
+                    "\"$name\"",
+                    'director/' . str_replace('_', '-', $type),
+                    ['name' => $name],
+                    ['title' => $this->translate('Jump to this object')]
+                );
             } else {
                 $object = Link::create(
                     "\"$name\"",
